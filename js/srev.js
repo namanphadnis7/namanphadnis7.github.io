7bd76dbc6530e4bf54558cd26b48d85bdee420e0
Naman Phadnis <namanphadnis7@gmail.com>
2023-07-15 16:35:21 -0700

    major home page changes

--- js/srev.js
+++ js/srev.js
@@ -7,12 +7,19 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .button, .skills__img, .pill',{delay: 400}); 
+sr.reveal('.home_data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home_img, .about__subtitle, .about__text, .button, .skills__img, .pill',{delay: 400}); 
 sr.reveal('.home__social-icon, .scroll-down',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input,',{interval: 200}); 
 sr.reveal('.about section',{}); 
 sr.reveal('.extra',{delay: 50}); 
+
+/* sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .button, .skills__img, .pill',{delay: 400}); 
+sr.reveal('.home__social-icon, .scroll-down',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input,',{interval: 200}); 
+sr.reveal('.about section',{}); 
+sr.reveal('.extra',{delay: 50}); */
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
